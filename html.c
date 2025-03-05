496c79879118c7b37c8eca122a8005c69c39559a
Patchipulusujaswitha <patchipulusu.jaswitha@s.amity.edu>
2025-03-05 13:57:17 +0530

    Add files via upload

---
+++ html.c
@@ -0,0 +1,28 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <table>
+    <tr>
+        <td> name:</td>
+        <td>jaswitha</td>
+    </tr>
+    <tr>
+        <td> age:</td>
+        <td>18</td>
+    </tr>
+    <tr>
+        <td> name of the university:</td>
+        <td> Amity University </td>
+    </tr>
+    <tr>
+        <td> branch:</td>
+        <td> btech AIML</td>
+    </tr>
+   </table> 
+</body>
+</html>
